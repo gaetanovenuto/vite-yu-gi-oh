b67fdcf8a43d4160694dffc6b0b38c38ba00b289
Gaetano Venuto <gaetanovenuto99@live.it>
2024-09-12 15:13:55 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script>
+/* 
+    Per importare ed utilizzare un componente dentro un altro devo SEMPRE seguire questi 3 passi:
+    1) Importazione del componente
+    2) Dichiarazione del componente
+    3) Utilizzo del componente
+*/
+
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  components: {
+    
+  },
+  created() {
+    axios
+      .get('URL')
+      .then()
+  }
+}
+</script>
+
+<!-- All'interno del template ci va un solo elemento -->
+<template> 
+  <div>
+    
+  </div>
+</template>
+
+<style lang="scss" scoped>
+// Import all of Bootstrap's CSS
+@import "~bootstrap/scss/bootstrap";
+</style>
